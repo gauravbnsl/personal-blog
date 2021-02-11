f02279a2cc17af50a2f437408ea9457755bf8d12
Gaurav Bansal <gaurav.bansal67@hotmail.com>
2021-02-11 14:25:23 +0530

    feat: added the home banner

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
-
+import { HomeRoutingModule } from './home-routing.module';
+import { BannerComponent } from './../../components/banner/banner.component';
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, BannerComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
