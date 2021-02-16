c4a9ffeb3370e556cd1aed5bbb27ea527b076bd0
Gaurav Bansal <gauravbansal67@hotmail.com>
2021-02-17 02:41:19 +0530

    feat: added content to widget component

---
+++ src/app/components/timeline-widget/timeline-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-widget',
+  templateUrl: './timeline-widget.component.html',
+  styleUrls: ['./timeline-widget.component.scss']
+})
+export class TimelineWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { BlogCardComponent } from './../../components/blog-card/blog-card.component';
-
+import { TimelineWidgetComponent } from './../../components/timeline-widget/timeline-widget.component'
 @NgModule({
-  declarations: [HomeComponent, BlogCardComponent],
+  declarations: [HomeComponent, BlogCardComponent, TimelineWidgetComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
